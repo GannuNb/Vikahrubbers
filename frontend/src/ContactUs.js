559449dc77763cd5db004471b65f67ece22fbefd
GannuNb <nbganesh2002@gmail.com>
2026-01-31 17:20:01 +0530

    mapsrc

--- frontend/src/ContactUs.js
+++ frontend/src/ContactUs.js
@@ -93,7 +93,6 @@ const ContactUs = () => {
             <iframe
               title="Patel Towers Location"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3049.299318499121!2d78.5549449239801!3d17.368280153357624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99eeab8a7795%3A0x89760778e2498478!2sPatel%20Towers!5e1!3m2!1sen!2sin!4v1749105069085!5m2!1sen!2sin"
-              width="100%"
               height="250"
               allowFullScreen=""
               loading="lazy"
@@ -109,7 +108,7 @@ const ContactUs = () => {
           <div>
             <iframe
               title="SSR Granites Rampur Warangal"
-              src="https://www.google.com/maps?q=17.9371379450896,79.43055189200328&z=15&output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d286.3436193033012!2d79.4626721!3d17.9473229!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a33537643652aeb%3A0xf82bde918a1fc2da!2sPrime%20India%20Recycling%20Centre!5e1!3m2!1sen!2sin!4v1769859453385!5m2!1sen!2sin" 
               width="100%"
               height="250"
               allowFullScreen=""
